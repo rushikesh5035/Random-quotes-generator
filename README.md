86c5747bb3984def419d7a81cb9841aadd519266
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2024-08-10 15:55:46 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# In this project we create a simple random qoutes generator using API in js
--- app.js
+++ app.js
@@ -15,32 +15,3 @@ async function getQuotes() {
   // console.log(res.data);
   return res.data;
 }
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// --Another Way
-
-// async function getAuthor() {
-//   let author = await axios.get(url);
-//   console.log(author.data.author);
-//   return author.data.author;
-// }
-
-// const quoteText = document.querySelector(".quote");
-// let authorName = document.querySelector(".author_name");
-// let quotebtn = document.querySelector("button");
-
-// quotebtn.addEventListener("click", randomQuote);
-
-// //random quote function
-// function randomQuote() {
-//   // fetching random quotes/ data from the API and parsing it into js object
-//   fetch("https://dummyjson.com/quotes/random")
-//     .then((res) => res.json())
-//     .then((result) => {
-//       console.log(result.quote);
-//       console.log(result.author);
-//       quoteText.innerText = result.quote;
-//       authorName.innerText = result.author;
-//     });
-// }
